65c6137078f3caad480b0558d2cbdc3b46a6b61e
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-02-13 23:07:18 +0530

    CSS styling Done

--- Guess Board Game/script.js
+++ Guess Board Game/script.js
@@ -1,33 +1,12 @@
-let guess = document.querySelector("guess");
-let btn = document.querySelector("btn");
-console.log(guess)
+let btn = document.getElementById("btn");
 
 
-
-
-
-
-btn.addEventListener("clicked", function guessBoardGame() {
-    const word = "HELLO";
-    const maxGuesses = 5;
-    let correctGuesses = [];
-    let incorrectGuesses = [];
-  
-    while (correctGuesses.length < word.length && incorrectGuesses.length < maxGuesses) {
-    //   let userInput = prompt(`Enter a letter. Correct guesses: ${correctGuesses.join('')} Incorrect guesses: ${incorrectGuesses.join(', ')}`);
-      if (word.includes(guess.value)) {
-        correctGuesses.push(guess.value);
-      } else {
-        incorrectGuesses.push(guess.value);
-      }
-    }
-  
-    if (correctGuesses.length === word.length) {
-      alert("You win! The word was " + word);
+btn.addEventListener("click", function () {
+    if (this.innerHTML === "Start Game") {
+      this.innerHTML = "Submit";
+      this.style.backgroundColor = "red"
     } else {
-      alert("You lose. The word was " + word);
+      this.innerHTML = "Start Game";
+      this.style.backgroundColor = "blue"
     }
-  })
-
-
-  
+  });
