b022108641cb078770a74da15c96c08d470bfee3
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-02-16 17:42:20 +0530

    debugging done working now

--- PIANO Player/script.js
+++ PIANO Player/script.js
@@ -1,33 +1,33 @@
 // The keys and notes variables store the piano keys
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 const notes = [];
-keys.forEach(function(key){
-  notes.push(document.getElementById(key));
+keys.forEach(function (key) {
+    notes.push(document.getElementById(key));
 })
 
 // Write named functions that change the color of the keys below
-const keyPlay = function(event){
-  event.target.style.backgroundColor = "grey"
+const keyPlay = function (event) {
+    event.target.style.backgroundColor = "grey"
 }
 
-const keyReturn = function(event){
-  event.target.style.backgroundColor = ""
+const keyReturn = function (event) {
+    event.target.style.backgroundColor = ""
 }
 
 
 
 // Write a named function with event handler properties
-let eventAssignment = function(note){
-  note.onmousedown = function(){
-    keyPlay(event)
-  }
-  note.onmouseup = function(){
-    keyReturn(event)
-}
+let eventAssignment = function (note) {
+    note.onmousedown = function (event) {
+        keyPlay(event)
+    }
+    note.onmouseup = function (event) {
+        keyReturn(event)
+    }
 }
 
 // Write a loop that runs the array elements through the function
-notes.forEach(eventAassignment);
+notes.forEach(eventAssignment);
 
 // These variables store the buttons that progress the user through the lyrics
 let nextOne = document.getElementById('first-next-line');
@@ -41,21 +41,21 @@ let lastLyric = document.getElementById('column-optional');
 // These statements are "hiding" all the progress buttons, but the first one
 nextTwo.hidden = true;
 nextThree.hidden = true;
-startOver.hidden= true;
+startOver.hidden = true;
 
 // Write anonymous event handler property and function for the first progress button
-nextOne.onclick = function(){
-  nextTwo.hidden = false;
-  nextOne.hidden = true;
+nextOne.onclick = function () {
+    nextTwo.hidden = false;
+    nextOne.hidden = true;
 }
 
 document.getElementById("letter-note-five").innerHTML = "D"
 document.getElementById("letter-note-six").innerHTML = "C"
 
 // Write anonymous event handler property and function for the second progress button
-nextTwo.onclick = function(){
-  nextThree.hidden = false;
-  nextTwo.hidden = true;
+nextTwo.onclick = function () {
+    nextThree.hidden = false;
+    nextTwo.hidden = true;
 }
 
 document.getElementById("word-five").innerHTML = "DEAR"
@@ -69,10 +69,10 @@ document.getElementById("letter-note-six").innerHTML = "B"
 lastLyric.style.display = "inline-block"
 
 // Write anonymous event handler property and function for the third progress button
-nextThree.onclick = function(){
-  nextFour.hidden = false;
-  nextThree.hidden = true;
-  startOver.hidden = false;
+nextThree.onclick = function () {
+    nextFour.hidden = false;
+    nextThree.hidden = true;
+    startOver.hidden = false;
 }
 document.getElementById("word-one").innerHTML = "HAP-"
 document.getElementById("word-two").innerHTML = "PY"
@@ -91,19 +91,19 @@ document.getElementById("letter-note-six").innerHTML = "C"
 lastLyric.style.display = "none"
 
 // This is the event handler property and function for the startOver button
-startOver.onclick = function() {
-  nextOne.hidden = false;
-  startOver.hidden = true;
-   document.getElementById('word-one').innerHTML = 'HAP-';
-  document.getElementById('letter-note-one').innerHTML = 'G';
-  document.getElementById('word-two').innerHTML = 'PY';
-  document.getElementById('letter-note-two').innerHTML = 'G';
-  document.getElementById('word-three').innerHTML = 'BIRTH-';
-  document.getElementById('letter-note-three').innerHTML = 'A';
-  document.getElementById('word-four').innerHTML = 'DAY';
-  document.getElementById('letter-note-four').innerHTML = 'G';
-  document.getElementById('word-five').innerHTML = 'TO';
-  document.getElementById('letter-note-five').innerHTML = 'C';
-  document.getElementById('word-six').innerHTML = 'YOU!';
-  document.getElementById('letter-note-six').innerHTML = 'B';
+startOver.onclick = function () {
+    nextOne.hidden = false;
+    startOver.hidden = true;
+    document.getElementById('word-one').innerHTML = 'HAP-';
+    document.getElementById('letter-note-one').innerHTML = 'G';
+    document.getElementById('word-two').innerHTML = 'PY';
+    document.getElementById('letter-note-two').innerHTML = 'G';
+    document.getElementById('word-three').innerHTML = 'BIRTH-';
+    document.getElementById('letter-note-three').innerHTML = 'A';
+    document.getElementById('word-four').innerHTML = 'DAY';
+    document.getElementById('letter-note-four').innerHTML = 'G';
+    document.getElementById('word-five').innerHTML = 'TO';
+    document.getElementById('letter-note-five').innerHTML = 'C';
+    document.getElementById('word-six').innerHTML = 'YOU!';
+    document.getElementById('letter-note-six').innerHTML = 'B';
 }
